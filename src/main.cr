0f6d034a9f0feb2d80d851a203715906d40d9d79
Lucian Buzzo <lucian.buzzo@gmail.com>
2023-07-21 18:37:28 +0100

    fix flowchart representation
    Signed-off-by: Lucian Buzzo <lucian.buzzo@gmail.com>

--- src/main.cr
+++ src/main.cr
@@ -1,4 +1,34 @@
+def draw_dot(root : Node) : String
+  dot = "digraph {\n"
+  dot += "rankdir=LR;\n"
+
+  nodes, edges = trace(root)
+  nodes.each do |n|
+    uid = n.id.to_s
+    dot += "#{uid} [label=\"{ #{n.value} }\", shape=record];\n"
+    if n._op != ""
+      opLabel = n._op === "+" ? "plus" : "mul"
+      dot += "#{uid + opLabel} [label=\"#{n._op}\"];\n"
+      dot += "#{uid + opLabel} -> #{uid};\n"
+    end
+  end
+
+  edges.each do |n1, n2|
+    opLabel = n2._op === "+" ? "plus" : "mul"
+    dot += "#{n1.id.to_s} -> #{n2.id.to_s + opLabel};\n"
+  end
+
+  dot += "}\n"
+
+  dot
+end
+
+puts "\n\n============ Running ============\n"
+
 struct Node
+  @@id_counter = 0
+
+  property id : String
   property value
   property _children
   property _prev
@@ -6,6 +36,7 @@ struct Node
 
   def initialize(@value : Float32, @_children = [] of Node, @_op = "")
     @_prev = @_children
+    @id = "n#{(@@id_counter += 1)}"
   end
 
   def +(other : Node)
@@ -21,14 +52,23 @@ struct Node
   end
 end
 
-def to_dot(node : Node, file : IO)
-  node._children.each do |child|
-    file.puts "\"#{node._op} #{node.value}\" -> \"#{child._op} #{child.value}\";"
-    to_dot(child, file)
+def build(v : Node, nodes, edges)
+  unless nodes.includes?(v)
+    nodes.add(v)
+    v._prev.each do |child|
+      edges.add({child, v})
+      build(child, nodes, edges)
+    end
   end
 end
 
-puts "\n\n============ Running ============\n"
+def trace(root : Node) : Tuple(Set(Node), Set(Tuple(Node, Node)))
+  nodes, edges = Set(Node).new, Set(Tuple(Node, Node)).new
+
+  build(root, nodes, edges)
+
+  {nodes, edges}
+end
 
 a = Node.new 2.0
 b = Node.new -3.0
@@ -40,8 +80,7 @@ puts d
 
 puts d._op
 
-File.open("tree.dot", "w") do |file|
-  file.puts "digraph Tree {"
-  to_dot(d, file)
-  file.puts "}"
-end
+dot_str = draw_dot(d)
+
+# Write the output to 'tree.dot'
+File.write("tree.dot", dot_str)
